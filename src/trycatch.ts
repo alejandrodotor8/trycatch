6ae6922a93d86de2b6d74fb39f2dcdcaa8f93979
Alejandro Dotor <diego.dotor.ext@essity.com>
2025-10-20 16:13:51 -0500

    Feat: Refactor `tryCatch` to accept callback and introduce `tryCatchSync`

--- src/trycatch.ts
+++ src/trycatch.ts
@@ -1,10 +1,29 @@
 type Result<T, E = Error> = [E | null, T | null];
 
-export async function tryCatch<T, E = Error>(
-  promise: Promise<T>
-): Promise<Result<T, E>> {
+export async function tryCatch<
+  TCallback extends (...args: any[]) => Promise<any>,
+  E = Error
+>(
+  callback: TCallback,
+  ...args: Parameters<TCallback>
+): Promise<Result<Awaited<ReturnType<TCallback>>, E>> {
   try {
-    const data = await promise;
+    const data = await callback(...args);
+    return [null, data];
+  } catch (error) {
+    return [error as E, null];
+  }
+}
+
+export function tryCatchSync<
+  TCallback extends (...args: any[]) => any,
+  E = Error
+>(
+  callback: TCallback,
+  ...args: Parameters<TCallback>
+): Result<ReturnType<TCallback>, E> {
+  try {
+    const data = callback(...args);
     return [null, data];
   } catch (error) {
     return [error as E, null];
